98781f73164dfd1513a8103ae18916a4a3280d35
Lars Sjögreen <barse@helgo.net>
2016-03-28 22:38:37 +0200

    Adding inventory and stuff that can be picked up.

--- src/game/core.clj
+++ src/game/core.clj
@@ -3,43 +3,56 @@
 (load "random")
 (load "rooms_and_map")
 
+(defrecord statecol [room user inventory])
+(defrecord userstate [name hp])
+(defrecord inventorystate [stuff])
+
 ; Use (refresh) to refresh in repl
 (use 'clojure.tools.namespace.repl)
 
 ; Command validation and quit
-(def valCommands #{"south" "north" "west" "east" "up" "down" "look" "help" "greet" "exit" "quit" "exits" })
-(defn invalid [room] (println "Invalid command") room)
+(def valCommands #{"south" "north" "west" "east" "up" 
+	"down" "look" "help" "greet" "exit" "quit" "exits" "inventory" "pickup" })
+(defn invalid [state] (println "Invalid command") state)
 (defn valC? [com] (contains? valCommands com))
 (defn quit? [x] (if (or (= "quit" x) (= "exit" x)) true false))
 
+(defn newroom [state room] (->statecol room (:user state) (:inventory state)))
+(defn addinv [state thing] (->statecol (:room state) (:user state) (->inventorystate (conj (:stuff (:inventory state)) thing))))
+
 ; Game commands
-(defn look [room] (println (:text room)) room)
-(defn roomname [room] (println (:name room)) room)
-(defn help [room] (println valCommands) room)
-(defn exits [room] (println (keys (worldmap (:symbol room)))) room)
-(defn greet [room] (println "This is a game called Game.") room)
+(defn look [state] (println (:text (:room state))) state)
+(defn help [state] (println valCommands) state)
+(defn exits [state] (println (keys (worldmap (:symbol (:room state))))) state)
+(defn inventory [state] (println "You carry: " (interpose "-" (map things (:stuff (:inventory state))))) state)
+(defn pickup [state] (if (not (nil? (:thing (:room state)))) (addinv state (:thing (:room state))) (do (println "Nothing to pick up") state)))
+
+(defn roomname [state] (println (:name (:room state))) state)
+(defn greet [state] (println "This is a game called Game.") state)
 
 ; Direction valCommands
 (defn invaliddir? [room dir] (= nil (dir ((:symbol room) worldmap))))
-(defn godir [room dir] (if (invaliddir? room dir) (do (println "You can't go that way") room) (roomname (dir ((:symbol room) worldmap)))))
-(defn south [room] (godir room :south))
-(defn north [room] (godir room :north))
-(defn west [room] (godir room :west))
-(defn east [room] (godir room :east))
-(defn down [room] (godir room :down))
-(defn up [room] (godir room :up))
+(defn godir [state dir] (if (invaliddir? (:room state) dir) (do (println "You can't go that way") state) (roomname (newroom state (dir ((:symbol (:room state)) worldmap))))))
+(defn south [state] (godir state :south))
+(defn north [state] (godir state :north))
+(defn west [state] (godir state :west))
+(defn east [state] (godir state :east))
+(defn down [state] (godir state :down))
+(defn up [state] (godir state :up))
 
 ; Another dispatcher method
 (defn doro [act parm] ((ns-resolve 'game.core (symbol act)) parm))
 
-(defn input [room imp] (loop [rum room inpot imp]
+(defn input [state imp] (loop [stat state inpot imp]
 	(if (valC? inpot)
 	(if (quit? inpot) (println "hasta la vista!") 
-		(recur (doro inpot rum) (read-line)))
-	(recur (doro "invalid" rum) (read-line)))))
+		(recur (doro inpot stat) (read-line)))
+	(recur (doro "invalid" stat) (read-line)))))
+
+(def startState (->statecol kitchen (->userstate "Frankenstein" 20) (->inventorystate [])))
 
 (defn -main
   "This is the main entrypoint to the game called Game."
   [& args]
-  (input kitchen "greet")
+  (input startState "greet")
 )
--- src/game/rooms_and_map.clj
+++ src/game/rooms_and_map.clj
@@ -1,18 +1,26 @@
 ; Rooms and map
-(defrecord room [name text symbol])
+(defrecord room [name text symbol thing])
 
-(def woods (->room "Woods" "Trees and shrubbery. And pine cones." :woods))
-(def livingroom (->room "Living room" "A nice cozy couch and a bookcase." :livingroom))
-(def kitchen (->room "Kitchen" "This looks like a kitchen. It's hot and it you can smell tasty, tasty food." :kitchen))
-(def cellar (->room "Cellar" "It's dark and terrifying here. It smells of moss and mold." :cellar))
-(def corridor (->room "Corridor" "This is a long and boring corridor" :corridor))
-(def office (->room "Office" "A dimly lit room with a desk, a chair and a small bookcase", :office))
-(def frontlawn (->room "Front lawn" "The lawn in front of a small house" :frontlawn))
+(def things { :keyT "A key" :mapT "An old map"})
+
+(def woods (->room "Woods" "Trees and shrubbery. And pine cones." :woods nil))
+(def livingroom (->room "Living room" "A nice cozy couch and a bookcase." :livingroom nil))
+(def kitchen (->room "Kitchen" "This looks like a kitchen. It's hot and it you can smell tasty, tasty food." :kitchen nil))
+(def cellar (->room "Cellar" "It's dark and terrifying here. It smells of moss and mold." :cellar :keyT ))
+(def corridor (->room "Corridor" "This is a long and boring corridor." :corridor nil))
+(def office (->room "Office" "A dimly lit room with a desk, a chair and a small bookcase.", :office nil))
+(def frontlawn (->room "Front lawn" "The lawn in front of a small house." :frontlawn nil))
+(def clearing (->room "A clearing" "The forest opens up into a small clearing. With a pond." :clearing :mapT ))
+(def road (->room "A forest road" "A dwindling forest road." :road nil ))
+(def bridge (->room "A bridge" "A rickety bridge over a fast flowing stream in the forest." :bridge nil ))
 
 (def worldmap { 
 	:kitchen { :room kitchen :south livingroom :north woods :west corridor :east frontlawn } 
 	:livingroom { :room livingroom :north kitchen :down cellar } 
-	:woods { :room woods :south kitchen }
+	:woods { :room woods :south kitchen :north clearing }
+	:clearing { :room clearing :south woods :west road }
+	:road { :room road :east clearing :west bridge }
+	:bridge { :room bridge :east road }
 	:corridor { :room corridor :east kitchen :west office }
 	:cellar { :room cellar :up livingroom }
 	:office { :room office :east corridor }
