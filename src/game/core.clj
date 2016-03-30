078281410c91958a23c027f515acfed2a43a2ac2
Lars Sjögreen <barse@helgo.net>
2016-03-30 19:35:40 +0200

    Starting to create functionality to enable verb noun commands. One more room. One more object.

--- src/game/core.clj
+++ src/game/core.clj
@@ -1,7 +1,11 @@
 (ns game.core (:gen-class))
+(require '[clojure.string :as str])
+(use 'clojure.pprint)
 
 (load "random")
 (load "rooms_and_map")
+
+(def version "1.0")
 
 (defrecord statecol [room user inventory])
 (defrecord userstate [name hp])
@@ -12,23 +16,25 @@
 
 ; Command validation and quit
 (def valCommands #{"south" "north" "west" "east" "up" 
-	"down" "look" "help" "greet" "exit" "quit" "exits" "inventory" "pickup" })
+	"down" "look" "help" "greet" "exit" "quit" "exits" "inventory" "pickup" "status" "debug" })
 (defn invalid [state] (println "Invalid command") state)
-(defn valC? [com] (contains? valCommands com))
+(defn valC? [com] (contains? valCommands (first (str/split com #" "))))
 (defn quit? [x] (if (or (= "quit" x) (= "exit" x)) true false))
 
 (defn newroom [state room] (->statecol room (:user state) (:inventory state)))
 (defn addinv [state thing] (->statecol (:room state) (:user state) (->inventorystate (conj (:stuff (:inventory state)) thing))))
+(defn debug [state] (pprint state) state)
 
 ; Game commands
-(defn look [state] (println (:text (:room state))) state)
+(defn look [state] (println (-> state :room :text)) state)
 (defn help [state] (println valCommands) state)
 (defn exits [state] (println (keys (worldmap (:symbol (:room state))))) state)
 (defn inventory [state] (println "You carry: " (interpose "-" (map things (:stuff (:inventory state))))) state)
 (defn pickup [state] (if (not (nil? (:thing (:room state)))) (addinv state (:thing (:room state))) (do (println "Nothing to pick up") state)))
-
+(defn status [state] (do (println "\n" (-> state :user :name)) (println "Hp: " (-> state :user :hp)) (inventory state)))
 (defn roomname [state] (println (:name (:room state))) state)
-(defn greet [state] (println "This is a game called Game.") state)
+(defn greet [state] (println "This is a game called Game.  ---  ( Version" version ")") state)
+;(defn prip [state])
 
 ; Direction valCommands
 (defn invaliddir? [room dir] (= nil (dir ((:symbol room) worldmap))))
@@ -41,7 +47,7 @@
 (defn up [state] (godir state :up))
 
 ; Another dispatcher method
-(defn doro [act parm] ((ns-resolve 'game.core (symbol act)) parm))
+(defn doro [act state] ((ns-resolve 'game.core (symbol act)) state))
 
 (defn input [state imp] (loop [stat state inpot imp]
 	(if (valC? inpot)
--- src/game/rooms_and_map.clj
+++ src/game/rooms_and_map.clj
@@ -1,12 +1,13 @@
 ; Rooms and map
 (defrecord room [name text symbol thing])
 
-(def things { :keyT "A key" :mapT "An old map"})
+(def things { :keyT "A key" :mapT "An old map" :swordT "A shiny sword" })
 
 (def woods (->room "Woods" "Trees and shrubbery. And pine cones." :woods nil))
 (def livingroom (->room "Living room" "A nice cozy couch and a bookcase." :livingroom nil))
 (def kitchen (->room "Kitchen" "This looks like a kitchen. It's hot and it you can smell tasty, tasty food." :kitchen nil))
 (def cellar (->room "Cellar" "It's dark and terrifying here. It smells of moss and mold." :cellar :keyT ))
+(def rootcellar (->room "Root cellar" "This is were the food is kept. Cold, damp and filled with turnips and a sword." :rootcellar :swordT ))
 (def corridor (->room "Corridor" "This is a long and boring corridor." :corridor nil))
 (def office (->room "Office" "A dimly lit room with a desk, a chair and a small bookcase.", :office nil))
 (def frontlawn (->room "Front lawn" "The lawn in front of a small house." :frontlawn nil))
@@ -15,13 +16,14 @@
 (def bridge (->room "A bridge" "A rickety bridge over a fast flowing stream in the forest." :bridge nil ))
 
 (def worldmap { 
-	:kitchen { :room kitchen :south livingroom :north woods :west corridor :east frontlawn } 
+	:kitchen { :room kitchen :south livingroom :north woods :west corridor :east frontlawn }
 	:livingroom { :room livingroom :north kitchen :down cellar } 
 	:woods { :room woods :south kitchen :north clearing }
 	:clearing { :room clearing :south woods :west road }
 	:road { :room road :east clearing :west bridge }
 	:bridge { :room bridge :east road }
 	:corridor { :room corridor :east kitchen :west office }
-	:cellar { :room cellar :up livingroom }
+	:cellar { :room cellar :up livingroom :north rootcellar }
+	:rootcellar { :room rootcellar :south cellar }
 	:office { :room office :east corridor }
 	:frontlawn { :room frontlawn :west kitchen }})
