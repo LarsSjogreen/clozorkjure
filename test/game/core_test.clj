5aa1b5a2339938ba572978790b4501a360b3a399
Lars Sjögreen <barse@helgo.net>
2016-04-02 22:19:34 +0200

    Adding a few tests. There is one failing right now. A few more to come.

--- test/game/core_test.clj
+++ test/game/core_test.clj
@@ -2,6 +2,24 @@
   (:require [clojure.test :refer :all]
             [game.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest standard-test
+  (testing "Should always work"
+    (is (= 1 1))))
+
+(deftest commandValidation
+	(testing "Exists in list shoud be true"
+		(is (valC? "help")))
+
+	(testing "Does not exist should not be true"
+		(is (not (valC? "hulp"))))
+
+	(testing "Capital characters should still be true"
+		(is (valC? "HELP"))))
+
+(deftest quitQuits
+	(testing "Quit quits"
+		(is (quit? "quit")))
+	(testing "Exit quits"
+		(is (quit? "exit")))
+	(testing "Stop doesn't quit"
+		(is not (quit? "stop"))))
