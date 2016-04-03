f1de17f7f48a22908812104889f29919fa65b966
Lars Sjögreen <lars.sjogreen@akvo.org>
2016-04-03 22:01:36 +0200

    Added another room.

--- src/game/rooms_and_map.clj
+++ src/game/rooms_and_map.clj
@@ -1,13 +1,14 @@
 ; Rooms and map
 (defrecord room [name text symbol thing])
 
-(def things { :keyT "A key" :mapT "An old map" :swordT "A shiny sword" })
+(def things { :keyT "A key" :mapT "An old map" :swordT "A shiny sword" :mushroomT "A tiny blue glowing mushroom with red spots"})
 
 (def woods (->room "Woods" "Trees and shrubbery. And pine cones." :woods nil))
 (def livingroom (->room "Living room" "A nice cozy couch and a bookcase." :livingroom nil))
 (def kitchen (->room "Kitchen" "This looks like a kitchen. It's hot and it you can smell tasty, tasty food." :kitchen nil))
 (def cellar (->room "Cellar" "It's dark and terrifying here. It smells of moss and mold." :cellar :keyT ))
 (def rootcellar (->room "Root cellar" "This is were the food is kept. Cold, damp and filled with turnips and a sword." :rootcellar :swordT ))
+(def cave (->room "An undeground cave" "This cave is the size of a small house. Impressive!" :cave :mushroomT ))
 (def corridor (->room "Corridor" "This is a long and boring corridor." :corridor nil))
 (def office (->room "Office" "A dimly lit room with a desk, a chair and a small bookcase.", :office nil))
 (def frontlawn (->room "Front lawn" "The lawn in front of a small house." :frontlawn nil))
@@ -24,6 +25,7 @@
 	:bridge { :room bridge :east road }
 	:corridor { :room corridor :east kitchen :west office }
 	:cellar { :room cellar :up livingroom :north rootcellar }
-	:rootcellar { :room rootcellar :south cellar }
+	:cave { :room cave :up rootcellar }
+	:rootcellar { :room rootcellar :south cellar :down cave }
 	:office { :room office :east corridor }
 	:frontlawn { :room frontlawn :west kitchen }})
